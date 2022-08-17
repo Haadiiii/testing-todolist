a18a222a2c1b66275654dd29f7d1469c9fa56c24
Newton <altontonnalumasa@gmail.com>
2022-08-17 18:56:14 +0300

    add unit testing for clear all completed items

--- modules.test.js
+++ modules.test.js
@@ -1,4 +1,5 @@
 import showList from './src/modules/showlist';
+import removeSelected from './src/modules/clearCompletedTask'; 
 
 test('This is for editing items', () => {
     expect(showList.edit).toBe(showList.edit);
@@ -6,4 +7,8 @@ test('This is for editing items', () => {
 
 test("this is for the checkbox status", () => {
     expect(showList.checkbox).toBe(showList.checkbox)
+})
+
+test("this is for the clear all status", () => {
+    expect(removeSelected).toBe(removeSelected)
 })
