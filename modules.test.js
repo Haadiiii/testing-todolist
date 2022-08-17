46d91712805269effab87cc9b991d6dc9747462b
Hamid Ali <hammali99@gmail.com>
2022-08-17 20:44:45 +0500

    Checkbox test created

--- modules.test.js
+++ modules.test.js
@@ -2,4 +2,8 @@ import showList from './src/modules/showlist';
 
 test('This is for editing items', () => {
     expect(showList.edit).toBe(showList.edit);
+});
+
+test("this is for the checkbox status", () => {
+    expect(showList.checkbox).toBe(showList.checkbox)
 })
